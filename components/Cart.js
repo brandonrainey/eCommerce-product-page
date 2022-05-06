6f2eeafe69761ef49210f2d2f2163eccc6073b5b
Brandon Rainey <brandonhrainey@gmail.com>
2022-05-06 18:13:01 -0400

    fixed missing display name

--- components/Cart.js
+++ components/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function ({ cartCount, setCartCount }) {
+export default function Cart({ cartCount, setCartCount }) {
 
 
   

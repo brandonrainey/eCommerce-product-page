1dab8a23109d05cef5730733c0a5dc6df52ec03c
Brandon Rainey <brandonhrainey@gmail.com>
2022-05-14 01:35:25 -0400

    fix maybe

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firbaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: 'AIzaSyBHpRnyeMJyK6EjjtcVUenSxB2jSy0RSxo',
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
